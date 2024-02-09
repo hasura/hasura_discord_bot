24e986593ad79b0117fb307f2b97c7507bec2552
Tristen Harr <tristenharr@gmail.com>
2024-02-08 22:57:00 -0600

    added docs for event_on_message.py

--- events/event_on_message.py
+++ events/event_on_message.py
@@ -4,12 +4,21 @@ import discord
 
 
 async def event_on_message(client: discord.Client, message: discord.Message):
+    """
+    Occurs when a message is sent
+    :param client: The discord client. (essentially a singleton instance of the bot)
+    :param message: The incoming message
+    :return: None
+    """
+    # If the message is from a voice channel, or other weird channel, discard it.
     if not isinstance(message.channel, discord.Thread):
         return
 
+    # If the message is not in the correct channel, discard it.
     if message.channel.parent_id not in CHANNELS:
         return
 
+    # If the bot authored the message, discard it.
     if message.author == client.user:
         return
 
@@ -18,6 +27,7 @@ async def event_on_message(client: discord.Client, message: discord.Message):
     is_new_thread = thread_id == message_id
     bot_mentioned = client.user in message.mentions
 
+    # If it is a new thread, insert into HGE.
     if is_new_thread:
         thread_message = await send_message_in_embed(message.channel,
                                                      title=CONTROLLER_TITLE,
@@ -39,6 +49,7 @@ async def event_on_message(client: discord.Client, message: discord.Message):
                               GRAPHQL_HEADERS)
     else:
         pass
+    # Insert the message into HGE
     variables = {
         "object": {
             "thread_id": thread_id,
@@ -55,6 +66,7 @@ async def event_on_message(client: discord.Client, message: discord.Message):
                           variables,
                           GRAPHQL_HEADERS)
     if bot_mentioned:
+        # Send a message to the user so they know the bot is working on a response.
         await message.channel.send(
             embed=discord.Embed(
                 title=get_random_loading_message(),
